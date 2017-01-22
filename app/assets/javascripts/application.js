c7d63b2d672c3fae913d9098a52d0e4dea30a5e6
An Li <anli@Ans-Mac-mini.local>
2017-01-22 15:28:29 +0800

    fix some bug & welcome/index completed

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,8 +13,8 @@
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
-//= require bootstrap
 //= require bootstrap/alert
 //= require bootstrap/dropdown
 //= require bootstrap/carousel
+//= require bootstrap/collapse
 //= require_tree .
